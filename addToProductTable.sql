15b6857d66e67bda852a7316c59b30b92459fd5d
Mamuna Anwar <Mamuna_Anwar@epam.com>
2023-03-03 12:22:36 +0100

    work in progress

---
+++ addToProductTable.sql
@@ -0,0 +1,5 @@
+INSERT INTO products value { 'id' :'dc7ddd5d-8278-42d7-a4b4-556ddad34b9c',
+    'title' :'Barbie',
+    'description' :'A barbie doll for kids',
+    'price' :30,
+    'count' :100 }
---
+++ addToStocksTable.sql
@@ -0,0 +1,2 @@
+INSERT INTO stocks value { 'product_id' :'dc7ddd5d-8278-42d7-a4b4-556ddad34b9c',
+    'count' :100 }
